31ab8ddcbde289e498fec84db9dcb202f921cd58
johnwallm <64194445+johnwallm@users.noreply.github.com>
2020-08-06 02:30:08 +0800

    Initial commit

---
+++ Login/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Login.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context = new TeraEntities())
+            {
+                bool isValid = context.UserEx.Any(x => x.Username == model.Username && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    return RedirectToAction("Index", "Registrations");
+                }
+                ModelState.AddModelError("", "Invalid Username or Password");
+                return View();
+            }
+
+            
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserEx model)
+        {   
+            using (var context = new TeraEntities())
+            {
+                context.UserEx.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Login/Controllers/RegistrationsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Login;
+
+namespace Login.Controllers
+{
+    [Authorize]
+    public class RegistrationsController : Controller
+    {
+        private TeraEntities db = new TeraEntities();
+
+        // GET: Registrations
+        
+        public ActionResult Index()
+        {
+            return View(db.Registration.ToList());
+        }
+
+        // GET: Registrations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registration.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // GET: Registrations/Create
+        [Authorize(Roles = "Admin, User")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Registrations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin, User")]
+        public ActionResult Create([Bind(Include = "ID,First_Name,Middle_Initial,Last_Name,Email,Username,Password")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Registration.Add(registration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(registration);
+        }
+
+        // GET: Registrations/Edit/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registration.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: Registrations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit([Bind(Include = "ID,First_Name,Middle_Initial,Last_Name,Email,Username,Password")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(registration);
+        }
+
+        // GET: Registrations/Delete/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registration.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: Registrations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Registration registration = db.Registration.Find(id);
+            db.Registration.Remove(registration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
