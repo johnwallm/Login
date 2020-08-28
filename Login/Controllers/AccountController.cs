50699d178f863349ace1be406ed9e8d26e279cf3
johnwallm <64194445+johnwallm@users.noreply.github.com>
2020-08-28 20:46:22 +0800

    D

--- Login/Controllers/AccountController.cs
+++ Login/Controllers/AccountController.cs
@@ -1,4 +1,4 @@
-﻿using Login.Models;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -13,84 +13,6 @@ namespace Login.Controllers
   
     public class AccountController : Controller
     {
-        // GET: Account
-        
-        public ActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Authorize(UserInformation user)
-        {
-            using (TeraEntities1 db = new TeraEntities1())
-            {
-                var userDetail = db.UserInformations.Where(x => x.Username == user.Username && x.Password == user.Password).FirstOrDefault();
-
-
-
-
-                if (userDetail == null)
-                {
-                    //user.LoginErrorMessage = "Invalid Username or Password";
-                    return View("Login", user);
-                }
-
-                else
-
-                {
-                    var UserInformationID = userDetail.UserInformationID;
-                    var UserTypeID = userDetail.UserTypeID;
-                    var Password = userDetail.Password;
-                    var Username = userDetail.Username;
-
-                    
-
-                    Session["UserInformationID"] = UserTypeID;
-                    Session["UserName"] = Username;
-                    Session["UserTypeID"] = UserTypeID;
-                    Session["Password"] = Password;
-
-                    if (UserTypeID == 1)
-                    {
-
-                        return RedirectToAction("AdminH", "Account");
-                    }
-
-                    else if (UserTypeID == 2)
-                    {
-
-                        return RedirectToAction("UserPage", "Account");
-                    }
-
-                    else
-                    {
-                        return RedirectToAction("Login", "Account");
-                    }
-                }
-
-            }
-
-
-        }
-
-        public ActionResult LogOut()
-        {
-            int UserInformationID = (int)Session["UserInformationID"];
-            Session.Abandon();
-            return RedirectToAction("Login", "Account");
-        }
-
-        public ActionResult Administrator()
-        {
-            return View();
-        }
-
-        public ActionResult UserPage()
-        {
-            return View();
-        }
-
         //for image saving
         public ActionResult AddImage()
         {
